3667110ebb7c377687765830862afa7bf587d2c2
Isac <isac.mamede14@gmail.com>
2023-05-31 21:32:52 -0300

    add sample area

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let hexadecimalInput = document.querySelector('#hexadecimal')
 colorInput.addEventListener('input', () =>{
     let color = colorInput.value;
     hexadecimalInput.value = color;
-    document.querySelector('h1').style.color = color;
+    document.querySelector('#sample').style.backgroundColor = color
 })
